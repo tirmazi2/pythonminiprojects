a2a5d2577e2afc2539b8af3537ee08e3f4df1754
Ali HamXa <alihamzatirmazi@gmail.com>
2020-06-08 13:52:56 +0500

    Updated

--- TheBoredlessTourist/The Boredless Tourist.py
+++
@@ -1,7 +0,0 @@
-#The Boredless Tourist
-#Developer: tirmazi2
-
-destination_list = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
-
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
-
